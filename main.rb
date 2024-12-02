5cbd6d3a7310cb0a517108be255c63b87586e1b8
paperflo <f.fichtner@papershift.com>
2024-12-02 11:52:10 +0100

    Add margins and inter font to PDF output

--- main.rb
+++ main.rb
@@ -1,7 +1,20 @@
 require 'grover'
 
 html_contract = File.read(ARGV[0])
-converter = Grover.new(html_contract, format: 'A4')
+converter = Grover.new(
+  html_contract,
+  format: 'A4',
+  margin: {
+    top: '2cm',
+    bottom: '2cm',
+    left: '1cm',
+    right: '1cm'
+  },
+  style_tag_options: [
+    { url: 'https://rsms.me/inter/inter.css' },
+    { path: 'inter-integration.css' }
+  ]
+)
 pdf_contract = converter.to_pdf
 
 File.write('grover-result.pdf', pdf_contract)
